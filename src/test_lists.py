fe22123e08a35b83b4c805505d5561ae300b78d2
MEskerod <112616513+MEskerod@users.noreply.github.com>
2022-11-17 20:49:06 +0100

    Didn't make all the test. They didn't behave as expected

--- src/test_lists.py
+++ src/test_lists.py
@@ -1,6 +1,21 @@
 """List tests."""
 
+from lists import (Link, length, drop, take, reverse)
 
-def test_me() -> None:
-    """Write tests if you want them."""
-    assert 2 + 2 == 4
+
+def test_length(): 
+    assert length(None) == 0
+    assert length(Link(1, Link(2, Link(3, Link(4, None))))) == 4
+    assert length(Link(1, Link(4, None))) == 2  
+
+
+def test_drop(): 
+    assert drop(None, 4) == None
+
+
+def test_take(): 
+    assert take(None, 4) == None 
+
+
+def test_reverse():
+    assert reverse(None) == None
